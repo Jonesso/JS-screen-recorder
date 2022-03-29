d9285d55326def8e086f5b55901f13dcb7ee74c4
s.pavlinina <sonya.pav25@gmail.com>
2022-03-29 21:31:12 +0300

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+const SERVER_URI = 'http://localhost:4000';
+
+let mediaRecorder = null;
+let dataChunks = [];
+
+function App() {
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
