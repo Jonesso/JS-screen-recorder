e221485c257072db645380ad42b15d3c4bd49c41
s.pavlinina <sonya.pav25@gmail.com>
2022-03-31 19:29:42 +0300

    Add: Initial idea

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useRef, useState} from "react";
+import {io} from "socket.io-client";
 
 const SERVER_URI = 'http://localhost:4000';
 
@@ -7,10 +8,99 @@ let mediaRecorder = null;
 let dataChunks = [];
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+    const username = useRef(`User_${Date.now().toString().slice(-4)}`);
+    const socketRef = useRef(io(SERVER_URI));
+    const videoRef = useRef();
+    const linkRef = useRef();
+
+    const [screenStream, setScreenStream] = useState();
+    const [voiceStream, setVoiceStream] = useState();
+    const [recording, setRecording] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    // New client connection
+    useEffect(() => {
+        socketRef.current.emit('user:connected', username.current);
+    }, []);
+
+    // Get video stream
+    useEffect(() => {
+        ;(async () => {
+            // check if getDisplayMedia is supported in browser
+            if (navigator.mediaDevices.getDisplayMedia) {
+                try {
+                    // get stream
+                    const _screenStream = await navigator.mediaDevices.getDisplayMedia({
+                        video: true
+                    });
+                    // update state
+                    setScreenStream(_screenStream);
+                } catch (e) {
+                    console.error('*** getDisplayMedia', e);
+                    setLoading(false);
+                }
+            } else {
+                console.warn('*** getDisplayMedia not supported');
+                setLoading(false);
+            }
+        })()
+    }, [])
+
+    // Get audio stream
+    useEffect(() => {
+        ;(async () => {
+            //  check if getUserMedia is supported in browser
+            if (navigator.mediaDevices.getUserMedia) {
+                // should get video stream first
+                if (screenStream) {
+                    try {
+                        // get stream
+                        const _voiceStream = await navigator.mediaDevices.getUserMedia({
+                            audio: true
+                        });
+                        // update state
+                        setVoiceStream(_voiceStream);
+                    } catch (e) {
+                        console.error('*** getUserMedia', e);
+                        setVoiceStream('unavailable');
+                    } finally {
+                        setLoading(false);
+                    }
+                }
+            } else {
+                console.warn('*** getUserMedia not supported');
+                setLoading(false);
+            }
+        })()
+    }, [screenStream]);
+
+
+    function startRecording() {
+        if (screenStream && voiceStream && !mediaRecorder) {
+            // TODO
+        }
+    }
+
+    const onClick = () => {
+        if (!recording) {
+            startRecording();
+        } else {
+            if (mediaRecorder) {
+                mediaRecorder.stop();
+            }
+        }
+    }
+
+    return (
+        <>
+            <h1>Screen Recording App</h1>
+            <video controls ref={videoRef}></video>
+            <a ref={linkRef}>Download</a>
+            <button onClick={onClick} disabled={!voiceStream}>
+                {!recording ? 'Start' : 'Stop'}
+            </button>
+        </>
+    );
 }
 
 export default App;
